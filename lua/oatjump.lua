bab1b34ab96cdb8b9632d94dc8ba59a3eb898d74
Shane Stephenson <stephenson.shane.a@gmail.com>
2024-10-31 23:38:09 -0400

    ...

--- lua/oatjump.lua
+++ lua/oatjump.lua
@@ -5,81 +5,78 @@ local config = {
     keymaps = {
         forward = "<C-l>",
         backward = "<C-h>",
+        forward_word = "<C-A-l>",
+        backward_word = "<C-A-h>",
     },
+    word_marker = "a-zA-Z0-9",
 }
 
-local function char_is_in_array(char, array)
-    for _, value in ipairs(array) do
-        if char == value then
-            return true
-        end
+function M.next_word(col, line, initial)
+  local line_length = vim.fn.strdisplaywidth(line)
+  if (col ~= 1) or (string.find(string.sub(line, 1, 1), M.word_start, 1) == nil) or not initial then
+    print(col, string.sub(line, 1, 1))
+    local next_pos = string.find(line, M.word_pat, col)
+    if next_pos then
+      return next_pos
     end
-    return false
+  else
+    return 0
+  end
 end
 
-local function get_separator_pattern()
-    local adjusted_separators = {}
-    local special_chars = {'-', '%', '^', '$', '.', '[', ']', '*', '+', '?', '(', ')', '{', '}'}
-    for _, separator in ipairs(M.separators) do
-        if char_is_in_array(separator, special_chars) then
-            table.insert(adjusted_separators, "%"..separator)
-        else
-            table.insert(adjusted_separators, separator)
-        end
+function M.prev_word(col, line, initial)
+  local line_length = vim.fn.strdisplaywidth(line)
+  if (col + 1 ~= line_length) or (string.find(string.sub(line, line_length, line_length), M.word_start, 1) == nil) or not initial then
+    print(col, line_length, string.sub(line, line_length, line_length))
+    local prev_word = string.find(string.reverse(string.sub(line, 1, col)), M.word_pat, 1)
+    if prev_word then
+      return col - prev_word - 1
     end
-    local separator_pattern = "[^"..table.concat(adjusted_separators, "").."]["..table.concat(adjusted_separators, "").."]"
-    return separator_pattern
+  else
+    return col
+  end
 end
 
-function M.jump_to_next()
-    local current_line = vim.api.nvim_get_current_line()
-    local cursor_position = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor_position[1], cursor_position[2]
-    local line_length = vim.fn.strdisplaywidth(current_line) - 1
-    if col >= line_length then
-        if row < vim.api.nvim_buf_line_count(0) then
-            vim.api.nvim_win_set_cursor(0, {row + 1, 0})
+function M.jump_to_next(initial)
+  local current_line = vim.api.nvim_get_current_line()
+  local cursor_position = vim.api.nvim_win_get_cursor(0)
+  local row, col = cursor_position[1], cursor_position[2]
+  local line_length = vim.fn.strdisplaywidth(current_line) - 1
+  local next_pos = M.next_word(col + 1, current_line, initial)
+  if next_pos then
+    vim.api.nvim_win_set_cursor(0, {row, next_pos})
+  else
+    if row < vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, {row + 1, 0})
+      M.jump_to_next(true)
+    end
+  end
+end
+
+function M.jump_to_prev(initial)
+  local current_line = vim.api.nvim_get_current_line()
+  local cursor_position = vim.api.nvim_win_get_cursor(0)
+  local row, col = cursor_position[1], cursor_position[2]
+    local prev_pos = M.prev_word(col, current_line, initial)
+    local other_prev_pos = M.prev_word(col + 1, current_line, initial)
+    if prev_pos then
+      if other_prev_pos then
+        if prev_pos < other_prev_pos then
+          prev_pos = other_prev_pos
         end
+      end
+    elseif other_prev_pos then
+      prev_pos = other_prev_pos
+    end
+    if prev_pos then
+      vim.api.nvim_win_set_cursor(0, {row, prev_pos})
     else
-        local search_pattern = get_separator_pattern()
-        local next_pos = string.find(current_line, search_pattern, col + 1)
-
-        if next_pos then
-            vim.api.nvim_win_set_cursor(0, {row, next_pos})
-        else
-            vim.api.nvim_win_set_cursor(0, {row, line_length})
-        end
-    end
-end
-
-function M.jump_to_prev()
-    local current_line = vim.api.nvim_get_current_line()
-    local cursor_position = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor_position[1], cursor_position[2]
-    if col == 0 then
-        if row > 1 then
-            local prev_line = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1]
-            local prev_line_length = vim.fn.strdisplaywidth(prev_line)
-            vim.api.nvim_win_set_cursor(0, {row - 1, prev_line_length})
-        end
-    else
-        local search_pattern = get_separator_pattern()
-        local search_index = col - 1
-        local prev_pos = nil
-
-        while search_index > 0 do
-            if string.sub(current_line, search_index, search_index + 1):match(search_pattern) then
-                prev_pos = search_index
-                break
-            end
-            search_index = search_index - 1
-        end
-
-        if prev_pos then
-            vim.api.nvim_win_set_cursor(0, {row, prev_pos})
-        else
-            vim.api.nvim_win_set_cursor(0, {row, 0})
-        end
+      if row > 1 then
+        local prev_line = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1]
+        local prev_line_length = vim.fn.strdisplaywidth(prev_line)
+          vim.api.nvim_win_set_cursor(0, {row - 1, prev_line_length})
+          M.jump_to_prev(true)
+      end
     end
 end
 
@@ -89,9 +86,11 @@ function M.setup(user_config)
         config[option] = value
     end
 
-    M.separators = config['separators']
-    vim.keymap.set({"n", "v"}, config["keymaps"]["forward"], function() M.jump_to_next() end, {})
-    vim.keymap.set({"n", "v"}, config["keymaps"]["backward"], function() M.jump_to_prev() end, {})
+    M.separators = config["separators"]
+    M.word_pat = "[^" .. config['word_marker'] .. "][" .. config['word_marker'] .. "]"
+    M.word_start = "[" .. config['word_marker'] .. "]"
+    vim.keymap.set({"n", "v"}, config["keymaps"]["forward"], function() M.jump_to_next(false) end, {})
+    vim.keymap.set({"n", "v"}, config["keymaps"]["backward"], function() M.jump_to_prev(false) end, {})
 end
 
 return M
